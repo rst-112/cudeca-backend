7d2002f934f8033615517f129340c74356f4224e
Raul Sanchez <rst112@uma.es>
2025-10-24 02:33:15 +0200

    no message

--- src/test/java/com/cudeca/CudecaBackendApplicationTests.java
+++ src/test/java/com/cudeca/CudecaBackendApplicationTests.java
@@ -3,8 +3,12 @@ package com.cudeca;
 import com.cudeca.config.MailConfigTest;
 import com.cudeca.testutil.IntegrationTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @Import(MailConfigTest.class)
 @IntegrationTest
 class CudecaBackendApplicationTests {
@@ -15,6 +19,11 @@ class CudecaBackendApplicationTests {
 
     @Test
     void mainMethodRuns() {
-        CudecaBackendApplication.main(new String[]{});
+        System.setProperty("spring.profiles.active", "test");
+
+        try (ConfigurableApplicationContext ctx =
+                     SpringApplication.run(CudecaBackendApplication.class)) {
+            assert ctx.isActive();
+        }
     }
 }
