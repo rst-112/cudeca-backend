5cae937367e987d677539bb4386f4216eec71d9b
Raul Sanchez <rst112@uma.es>
2025-12-11 20:48:57 +0100

    no message

--- src/main/java/com/cudeca/config/MailConfig.java
+++ src/main/java/com/cudeca/config/MailConfig.java
@@ -1,6 +1,7 @@
 package com.cudeca.config;
 
 import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -26,6 +27,7 @@ import java.util.Properties;
  */
 @Configuration
 @Profile("!test")
+@EnableConfigurationProperties(MailProperties.class)
 public class MailConfig {
 
     private final MailProperties mailProperties;
