1696d2c7a3638f198edd2e30166e56801691059e
Raul Sanchez <rst112@uma.es>
2025-12-18 06:45:50 +0100

    no message

--- src/main/java/com/cudeca/model/negocio/ArticuloSorteo.java
+++ src/main/java/com/cudeca/model/negocio/ArticuloSorteo.java
@@ -4,6 +4,7 @@ import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import java.math.BigDecimal;
@@ -12,6 +13,7 @@ import java.math.BigDecimal;
 @DiscriminatorValue("SORTEO")
 @Data
 @SuperBuilder
+@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class ArticuloSorteo extends ArticuloCompra {
 
